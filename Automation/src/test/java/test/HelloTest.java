2513d3009f2263f6752a78ea12c408699b6b391c
febat <febat@Feba_Thomas>
2024-12-06 19:03:29 +0530

    First Commit

---
+++ Automation/src/test/java/test/HelloTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.junit.Test;
+
+public class HelloTest {
+	@Test
+
+	public void testMethod()
+	{
+		System.out.println("I am in test");
+	}
+
+}
---
+++ Automation/src/test/java/test/TestHelloWorld.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.junit.Test;
+
+public class TestHelloWorld {
+	@Test
+	public void secondTEst()
+	{
+		System.out.println("hello");
+	}
+
+}
